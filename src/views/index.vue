f54790f573383c312851a5f6fd001e78f4c4e80b
chenBright <1021774709@qq.com>
2017-03-12 22:07:54 +0800

    feature_loading效果 (#2)

---
+++ src/views/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="index">
+        <img class="vue-logo" src="../assets/images/vue-logo.png">
+        <loading></loading>
+    </div>
+</template>
+
+<script>
+import loading from '../components/loading';
+
+export default {
+    name: 'index',
+    components: {
+        loading
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.vue-logo {
+    display: block;
+    margin: auto;
+    margin-top: 60px;
+}
+.el-loading {
+    display: block;
+    margin: auto;
+    margin-top: 20px;
+}
+</style>
